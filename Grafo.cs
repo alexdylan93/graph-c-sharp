dfa6d2f3b60c4789587140842855986b97e73b22
Omar Alejandro <omara.cruz@alumnos.udg.mx>
2019-08-04 13:46:07 -0500

    Update Grafo.cs
    Se cambio la clase circulo por clase Point, además se elimniaron métodos de la clase grafo y la clase vertice

--- Grafo.cs
+++ Grafo.cs
@@ -24,11 +24,11 @@ namespace etapa_1
 			
 		}
 		
-		public Grafo(List<Circle> lc)
+		public Grafo(List<Point> lp)
 		{
 			int cont = 0;
 			listaVertice = new List<Vertice>();
-			foreach (Circle c in lc){
+			foreach (Point p in lp){
 				Vertice v = new Vertice(c,cont);
 				listaVertice.Add(v);
 				cont++;
@@ -41,76 +41,18 @@ namespace etapa_1
 			return listaVertice;
 		}
 		
-		
-		
-		public void clear(){
-			listaVertice.Clear();
-		}
-		
-		public List<Arista> inicializarRecorridoAnchura(Vertice inicial){
-			List<Vertice> visitado = new List<Vertice>();
-			Queue<Vertice> cola = new Queue<Vertice>();
-			Queue<int> colaNivel = new Queue<int>();
-			bool primeraHoja = true;
-			colaNivel.Enqueue(1);
-			int altura = 1;
-			bool terminar = false;
-			
-			visitado.Add(inicial);
-			cola.Enqueue(inicial);
-			
-			List<Arista> la = recorridoProfundidad(visitado,cola,colaNivel,ref primeraHoja,ref altura,ref terminar);
-			
-			if(terminar)
-				return null;
-			return la;
-		}
-		
-		List<Arista> recorridoProfundidad(List<Vertice> visitados, Queue<Vertice> cola, Queue<int> colaNivel,ref bool primHoja, ref int altura, ref bool terminar){
-			
-			bool esHoja = true;
-			List<Arista> la = new List<Arista>();
-			if(terminar){
-				return la;
-			}
-			if(cola.Count != 0){
-				Vertice actual = cola.Dequeue();
-				int nivelActual = colaNivel.Dequeue();
-				foreach(Arista a in actual.getLA()){
-					if(!visitados.Exists(e => e.getEtiqueta() == a.getDestino().getEtiqueta())){
-						visitados.Add(a.getDestino());
-						cola.Enqueue(a.getDestino());
-						colaNivel.Enqueue(nivelActual+1);
-						la.Add(a);
-						esHoja = false; 
-					}
-				}
-				if(!primHoja && esHoja){
-						if(nivelActual != altura){
-							terminar = true;
-							return la;
-						}
-					}
-					if(esHoja && primHoja){
-						altura = nivelActual; 
-						primHoja = false;
-					}
-				la.AddRange(recorridoProfundidad(visitados,cola,colaNivel,ref primHoja,ref altura, ref terminar));
-			}
-			return la;
-		}
 	}
 	
 	
 	
 	
 	public class Vertice{
-		Circle c;
+		Point p;
 		int etiqueta;
 		List<Arista> listaArista;
 		
-		public Vertice(Circle c, int e){
-			this.c = c;
+		public Vertice(Point p, int e){
+			this.p = p;
 			this.etiqueta = e;
 			listaArista = new List<Arista>();
 		}
@@ -127,13 +69,6 @@ namespace etapa_1
 			return listaArista;
 		}
 		
-		public Arista getArista(Vertice d){
-			foreach(Arista a in listaArista){
-				if(a.getDestino().getEtiqueta() == d.getEtiqueta())
-					return a;
-			}
-			return null;
-		}
 		
 		public int getEtiqueta(){
 			return etiqueta;
